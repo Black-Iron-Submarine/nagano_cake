e277dad15f2edba9fe4cd6185dc83690e1046b2b
tkmaui <129600101+tkmaui@users.noreply.github.com>
2023-05-23 09:56:53 +0000

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :customers,skip: [:passwords], controllers: {
+  devise_for :customers, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
@@ -8,5 +8,26 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
 }
 
+ scope module: :public do
+    root to: "homes#top"
+    get '/about' => 'homes#about'
+    resources :items, only: [:index, :show]
+    resource :customers, only: [:show, :update]
+    get '/customers/is_deleted' => 'customers#is_deleted'
+    patch '/customers/destroy' => 'customers#destroy'
+    resources :cart_items, only: [:index, :update, :create]
+    delete '/cart_items/destroy' => 'cart_items#destroy_all'
+    resources :orders, only: [:new, :index, :show]
+    post '/orders/show' => 'oders#confirm'
+    get '/orders/index' => 'oders#complete'
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+ end
+ namespace :admin do
+    get '/admin' => 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+ end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
