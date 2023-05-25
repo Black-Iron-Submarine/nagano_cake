8192f5b0fc0ab394e43172661ceae6ff12698f21
tkmaui <129600101+tkmaui@users.noreply.github.com>
2023-05-25 12:17:08 +0000

    Additems

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,11 +1,15 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item = Item.new
   end
 
   def index
+    @item = 
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
@@ -15,5 +19,14 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :price_without_tax, :genre_id, :sales_status)
   end
 end
