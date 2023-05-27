bff9699f34f48f3ebfaea081015ae18109faeb8e
sasakiiori <126736985+sasakiiori@users.noreply.github.com>
2023-05-27 07:45:04 +0000

    tyumon_gamen_sekkei

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number])
+  end
 end
