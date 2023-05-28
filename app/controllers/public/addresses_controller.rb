fec1f16adf60176151ba5afddc7fa2f0f1ffc5b5
sasakiiori <126736985+sasakiiori@users.noreply.github.com>
2023-05-28 11:29:29 +0000

    address_haisousaki

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -17,6 +17,9 @@ class Public::AddressesController < ApplicationController
  end
 
  def update
+  address = Address.find(params[:id])
+  address.update(address_params)
+  redirect_to addresses_path
  end
 
  def destroy
